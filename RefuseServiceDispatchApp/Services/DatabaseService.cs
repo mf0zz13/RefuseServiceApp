c79a50cadc64905facd248a92388668306a2d39c
mf0zz13 <michael.foster31@outlook.com>
2024-09-26 15:31:26 -0400

    Created DataBase service to add dispatch records

---
+++ RefuseServiceDispatchApp/Services/DatabaseService.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using MySqlConnector;
+using RefuseServiceDispatchApp.Models;
+
+namespace RefuseServiceDispatchApp.Services;
+
+public class DatabaseService
+{
+    public async Task CreateDispatchRecord(DispatchRecord record)
+    {
+        var builder = new MySqlConnectionStringBuilder
+        {
+            Server = "mysqlmssa.mysql.database.azure.com",
+            Database = "guntherrefuse",
+            UserID = "applogin",
+            Password = "T3stP@ssw0rd",
+            SslMode = MySqlSslMode.Required,
+        };
+
+        using (var connection = new MySqlConnection(builder.ConnectionString))
+        {
+            await connection.OpenAsync();
+
+            string sql = "INSERT INTO dispatchrecords (DispatchDate, ServiceArea, Route, TruckNumber, Driver, HelperOne, HelperTwo, RefuseType) " +
+                        $"VALUES ('{record.Date.Year}-{record.Date.Month}-{record.Date.Day}', '{record.ServiceArea}', '{record.Route}', '{record.TruckNumber}', '{record.Driver}', '{record.HelperOne}', '{record.HelperTwo}', '{record.RefuseType}')";
+
+            connection.Execute(sql);
+        }
+
+    }
+}
+
