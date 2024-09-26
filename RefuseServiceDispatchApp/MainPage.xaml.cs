e49ae46343afa12deed44f0f2daaca8e3978ea4d
mf0zz13 <michael.foster31@outlook.com>
2024-09-26 09:27:59 -0400

    Renamed 'RefuseServiceDispatachApp' project file to 'RefuseServiceDispatchApp' to fix spelling error

---
+++ RefuseServiceDispatchApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RefuseServiceDispatchApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- RefuseServiceRazorClassLibrary/obj/Debug/net8.0/RefuseServiceRazorClassLibrary.AssemblyInfo.cs
+++ RefuseServiceRazorClassLibrary/obj/Debug/net8.0/RefuseServiceRazorClassLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RefuseServiceRazorClassLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cbb7fd709305a885932b38cfd1037ebb74e471f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be6db1625ef990874f3ebc708837b50fbfdd3971")]
 [assembly: System.Reflection.AssemblyProductAttribute("RefuseServiceRazorClassLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RefuseServiceRazorClassLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
