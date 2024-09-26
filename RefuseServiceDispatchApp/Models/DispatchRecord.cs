4a1eb85a0cf7891d698a3ec6530eee0d32a29285
mf0zz13 <michael.foster31@outlook.com>
2024-09-26 10:48:40 -0400

    Added Models and Updated _Imports to reflect new model folder

---
+++ RefuseServiceDispatchApp/Models/DispatchRecord.cs
@@ -0,0 +1,14 @@
+﻿namespace RefuseServiceDispatchApp.Models
+{
+    public class DispatchRecord
+    {
+        public DateTime Date { get; set; }
+        public int ServiceArea { get; set; }
+        public int Route { get; set; }
+        public int TruckNumber { get; set; }
+        public int Driver { get; set; }
+        public int HelperOne { get; set; }
+        public int HelperTwo { get; set; }
+        public int RefuseType { get; set; }
+    }
+}
---
+++ RefuseServiceDispatchApp/Models/Truck.cs
@@ -0,0 +1,9 @@
+﻿namespace RefuseServiceDispatchApp.Models
+{
+    public class Truck
+    {
+        public int TruckNumber { get; set; }
+        public bool NeedMaintenance { get; set; }
+        public DateTime RegistrationExpirationDate { get; set; }
+    }
+}
