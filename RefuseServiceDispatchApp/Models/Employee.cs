02c973be2e6ca83c3ba3ef79f31b6f9b46336fee
mf0zz13 <michael.foster31@outlook.com>
2024-09-27 23:24:58 -0400

    Updated var types for Models

--- RefuseServiceDispatchApp/Models/Employee.cs
+++ RefuseServiceDispatchApp/Models/Employee.cs
@@ -2,12 +2,10 @@
 
 public class Employee
 {
-    public enum RoleType { Thrower, Driver, Management }
-
-    public int EmployeeID { get; set; }
+    public string EmployeeID { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public RoleType Role { get; set; }
+    public string Role { get; set; }
     public DateTime? CdlExpiration { get; set; }
     public DateTime EmploymentStartDate { get; set; }
     public DateTime? EmploymentEndDate { get; set; }
